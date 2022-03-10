283bd2aeca43b7d26f3b2330c8cf1ab532e8fc06
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2022-03-10 00:08:20 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -7,17 +7,17 @@ def document_analyzer():
   #  text = file.read()
     
    
-    d =  { }
-    with open('document.txt','r', encoding = 'utf-8') as file:
-        for line in file:
-            print(line)
-            for word in line.split():
-                 word = word.lower()
-                 if word.isalnum():     
+    d =  { }    # initialize varibale for dictionary
+    with open('document.txt','r', encoding = 'utf-8') as file:         # how to for sure read file
+        for line in file:       # for every line in file
+            print(line)             #print lines
+            for word in line.split():       # line.split seperates by white space the words
+                 word=word.lower()          # word.lower() only counting lowercase, so we turned all uppercase to lower case
+                 if word.isalnum():         
                      d[word]= d.get(word,0)+1
                
     list = []
-    for w in sorted(d, key = d.get, reverse = True):   
+    for w in sorted(d, key = lambda key: (-d[key],key)):   
         list.append(w)
         #print(w,d[w])
     for i in range (5):

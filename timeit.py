42fea3c0f042130fe753380dd3ffb12eee223537
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2022-03-09 20:55:38 -0800

    Update timeit.py

--- timeit.py
+++ timeit.py
@@ -9,16 +9,16 @@ def calculate_time(func):
         start = time.time()
         temp = func()       # calling actual fsunction inside wrapper function
         end = time.time()
-        run_time = end - start
-        x = ("Total Time " + str(run_time) + "seconds")
+        timeToRun = end - start
+        x = ("Total Time " + str(timeToRun) + " seconds")
         print(x)
         return temp
     return wrapper
 
 @calculate_time
-def t1():
+def test():
     i = 10000000
     while i > 0:
         i -= 1
-t1()
+test()
 

68cfdb0753387c5831d3c3a8a481e01907859b6c
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2022-03-09 20:39:22 -0800

    Update timeit.py

--- timeit.py
+++ timeit.py
@@ -1,23 +1,21 @@
-#Create a file named timeit.py
-#Add the following import time to the top of the file
-#Create a decorator named calculate_time, decorator is a function that takes another function as its argument and returns another function. ALlow the extension of an original function
-#You can use time.time() to get the current time in seconds
-#After the decorator computes the time for the function to run it should print EXACTLY 'Total time X' where X is the amount of time it took to run in seconds.
 import time
+
+# decorator to calculate time to run a function
 def calculate_time(func):
     def wrapper():
-        start = time.time()
-        temp = func()       # calling actual fsunction inside wrapper function
-        end = time.time()
-        run_time = end - start
-        output = ("Total Time " + str(run_time) + "seconds")
-        print(output)
+        time_start = time.time()    #time right before function starts
+        temp = func()
+        time_end = time.time()      #time right after function ends
+        timeToRun = time_end - time_start
+        string = ("Total time " + str(timeToRun))
+        print(string)
         return temp
     return wrapper
 
 @calculate_time
-def t1():
+def test():
     i = 10000000
     while i > 0:
         i -= 1
-t1()
+
+test()

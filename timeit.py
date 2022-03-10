a93e285a6654d9226f94e219f3073adc5cdb49e6
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2022-03-09 20:57:31 -0800

    Update timeit.py

--- timeit.py
+++ timeit.py
@@ -1,12 +1,14 @@
 import time
+
+# decorator to calculate time to run a function
 def calculate_time(func):
     def wrapper():
-        start = time.time()
-        temp = func()       # calling actual fsunction inside wrapper function
-        end = time.time()
-        timeToRun = end - start
-        x = ("Total Time " + str(timeToRun) + " seconds")
-        print(x)
+        time_start = time.time()    #time right before function starts
+        temp = func()
+        time_end = time.time()      #time right after function ends
+        timeToRun = time_end - time_start
+        string = ("Total time " + str(timeToRun))
+        print(string)
         return temp
     return wrapper
 
@@ -15,5 +17,5 @@ def test():
     i = 10000000
     while i > 0:
         i -= 1
-test()
 
+test()

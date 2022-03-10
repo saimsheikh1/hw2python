9c2ad93e392a797432c7b2079826d8f2639b996d
saimsheikh1 <78775621+saimsheikh1@users.noreply.github.com>
2022-03-10 00:12:13 -0800

    Update document_analyzer.py

--- document_analyzer.py
+++ document_analyzer.py
@@ -10,7 +10,7 @@ def document_analyzer():
     d =  { }    # initialize varibale for dictionary
     with open('document.txt','r', encoding = 'utf-8') as file:         # how to for sure read file
         for line in file:       # for every line in file
-            print(line)             #print lines
+                        #print lines
             for word in line.split():       # line.split seperates by white space the words
                  word=word.lower()          # word.lower() only counting lowercase, so we turned all uppercase to lower case
                  if word.isalnum():         
